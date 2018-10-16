959421ae7c89700265426176ebaa99398e73b141
James Wilkins <contactme@jameswilkins.net>
2018-10-16 00:27:33 -0400

    Simple static file serving added.

--- Source/ide/server/index.ts
+++ Source/ide/server/index.ts
@@ -1,22 +1,46 @@
 import Koa from 'koa';
 import Router from 'koa-router';
+import koaBody from 'koa-body';
+import staticServer from 'koa-static';
 
 const port = 8080;
 const app = new Koa();
+const router = new Router();
+var currentPath = process.cwd(); // (this is the directory of the server, NOT any running script)
 
 app.use(async (ctx, next) => {
     // Log the request to the console
+    console.log('Server Path:', currentPath);
+    console.log('Current Module:', __filename);
+    console.log('Current Module Path:', __dirname);
     console.log('Url:', ctx.url);
     // Pass the request to the next middleware function
     await next();
 });
 
-const router = new Router();
-router.get('/*', async (ctx) => {
-    ctx.body = 'Hello World!';
-});
+app.use(staticServer("templates", { defer: false }));
+
+// app.use(koaBody());
+
+// app.use(async ctx => {
+// });
+
+router.post('/users', koaBody(),
+    (ctx) => {
+        console.log(ctx.request.body);
+        // => POST body
+        ctx.body = `Request Body: ${JSON.stringify(ctx.request.body)}`;
+    }
+);
 
 app.use(router.routes());
+
+// app.use(async (ctx, next) => {
+//     // Log the request to the console
+//     console.log('404');
+//     ctx.body = "The URL is not valid.";
+//     ctx.status = 404;
+// });
 
 app.listen(port);
 
