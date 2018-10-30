63bb3c8007df992e1f9a47fa58b964decc702299
James <contactme@jameswilkins.net>
2018-10-30 18:18:21 -0400

    Readme updates.

--- install_extensions.cmd
+++ install_extensions.cmd
@@ -1,9 +1,10 @@
 @echo off 
 REM The following is an auto-generated list of extensions used by this project. 
-REM Exported on: 2018-10-14 23:13:03.85 
+REM Exported on: Tue 10/30/2018 16:38:12.12 
 call code --install-extension alefragnani.project-manager 
 call code --install-extension bajdzis.vscode-database 
 call code --install-extension bierner.markdown-preview-github-styles 
+call code --install-extension christian-kohler.npm-intellisense 
 call code --install-extension christian-kohler.path-intellisense 
 call code --install-extension codezombiech.gitignore 
 call code --install-extension dbaeumer.vscode-eslint 
@@ -13,6 +14,7 @@ call code --install-extension donjayamanne.githistory
 call code --install-extension eamodio.gitlens 
 call code --install-extension eg2.tslint 
 call code --install-extension felipecaputo.git-project-manager 
+call code --install-extension file-icons.file-icons 
 call code --install-extension GregorBiswanger.json2ts 
 call code --install-extension hbenl.vscode-firefox-debug 
 call code --install-extension HookyQR.beautify 
@@ -29,6 +31,7 @@ call code --install-extension ms-vscode.vs-keybindings
 call code --install-extension msjsdiag.debugger-for-chrome 
 call code --install-extension msjsdiag.debugger-for-edge 
 call code --install-extension pmneo.tsimporter 
+call code --install-extension robertohuertasm.vscode-icons 
 call code --install-extension salbert.comment-ts 
 call code --install-extension streetsidesoftware.code-spell-checker 
 call code --install-extension stringham.move-ts 
