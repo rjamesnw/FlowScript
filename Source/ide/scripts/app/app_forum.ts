0c4f4c6a27e48e2ceb8022c073f78f0930f85ac4
James <contactme@jameswilkins.net>
2019-03-13 02:13:41 -0400

    Readme updates.

--- Source/ide/scripts/app/app_forum.ts
+++
@@ -1,7 +0,0 @@
-﻿/** this is called when the user selects to view the forum. */
-function loadForum() {
-    rootBodyViewContainer.createView("Forum", "/IDE/Forum").onloaded((view, req, ev) => {
-        view.buildViews(); // (search and parse views, if any)
-        view.show();
-    }).onerror((view, request, ev) => { view.contentElement.innerHTML = "Communication error - please try again."; }).send();
-}
